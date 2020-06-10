c30968c994a2b24550a2456294786c74d1a50cc2
Crowdbotics <team@crowdbotics.com>
2020-06-10 19:09:39 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/sanctioned_by_shay_18003/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sanctioned_by_shay_18003;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "sanctioned_by_shay_18003";
+    }
+}
